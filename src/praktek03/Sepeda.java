021de74900bb53408149a47f5bf7980a9d4105fa
nabiila zahida <35827020+nabiilazahida@users.noreply.github.com>
2018-04-08 10:16:10 +0800

    Membuat atribut

--- src/praktek03/Sepeda.java
+++ src/praktek03/Sepeda.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package praktek03;
 
-/**
- *
- * @author Windows 10 Pro
- */
 public class Sepeda {
-    
+    String merk ;
+    String warna ;
+    double harga;
 }
